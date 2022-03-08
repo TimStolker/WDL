d4e45666c1d293aa1ab94bac6f3db2213c89682a
TimStolker <60575792+TimStolker@users.noreply.github.com>
2022-03-08 21:42:15 +0100

    Added more nodes
    Added the var, assign, conditional, if/else and function nodes. Also fixed mathematic operation bug

--- src/classparser.py
+++ src/classparser.py
@@ -1,23 +1,23 @@
 from classtoken import *
-
+from typing import Tuple, Union
 #########################################
 # NODES
 #########################################
 
 
 class NumberNode:
-    def __init__(self, token: list):
+    def __init__(self, token: Token):
         self.token = token
 
     def __str__(self) -> str:
-        return f'{self.token[1]}'
+        return f'{self.token}'
 
     def __repr__(self) -> str:
         return self.__str__()
 
 
 class BinaryOperationNode:
-    def __init__(self, left_node: NumberNode, operation_token: NumberNode, right_node: NumberNode):
+    def __init__(self, left_node: NumberNode, operation_token: Token, right_node: NumberNode):
         self.left_node = left_node
         self.operation_token = operation_token
         self.right_node = right_node
@@ -28,16 +28,57 @@ class BinaryOperationNode:
     def __repr__(self) -> str:
         return self.__str__()
 
-class UnaryOperationNode:
-    def __init__(self, op_token, node):
-        self.operation_token = op_token
-        self.node = node
+
+class VarNode:
+    def __init__(self, token: Token):
+        self.token = token
+        self.value = token.value
 
     def __str__(self) -> str:
-        return f'({self.operation_token}, {self.node})'
+        return f'Var: {self.value}'
 
-    def __repr__(self) -> str:
-        return self.__str__()
+
+class AssignNode:
+    def __init__(self, left: VarNode, operation: Token, right: NumberNode) -> None:
+        self.left = left
+        self.token = self.operation = operation
+        self.right = right
+
+    def __str__(self) -> str:
+        return f'Assign: ({self.left}, {self.operation.value}, {self.right})'
+
+
+class ConditionalNode:
+    def __init__(self, left: Union[VarNode, NumberNode], condition: Token, right :Union[VarNode, NumberNode]) -> None:
+        self.left = left
+        self.condition = condition
+        self.right = right
+
+    def __str__(self) -> str:
+        return f'Conditional: ({self.left}, {self.condition}, {self.right})'
+
+
+class IfElseNode:
+    def __init__(self, condition: ConditionalNode, block: list, else_node: Token) -> None:
+        self.condition = condition
+        self.if_block = block
+        self.else_node = else_node
+
+    def __str__(self) -> str:
+        return f'IfElse: ({self.condition})'
+
+
+class FunctionNode:
+    def __init__(self, function_name: str, arguments: list, block: list, return_type: Token):
+        self.function_name = function_name
+        self.arguments = arguments
+        self.code_block = block
+        self.return_type = return_type
+
+    def __str__(self) -> str:
+        return f'Function: {self.function_name}, {self.return_type}'
+
+Node = Union[NumberNode,BinaryOperationNode]
 
 #########################################
 # PARSER
@@ -48,47 +89,47 @@ class Parser:
     def __init__(self, tokens: list) -> None:
         self.tokens = tokens
 
-    def parse(self, index: int, tokens: list):
-        result = self.expression(index, tokens)
+    def parse(self, index: int, tokens: list) -> Node:
+        result, index = self.expression(index, tokens)
         return result
 
-    def factor(self, index: int, tokens: list):
+    def factor(self, index: int, tokens: list) -> Tuple[Node,int]:
+        # The factor is the node that will be used in a term
+
         # current token
         token = tokens[index]
 
-        # Check for + or -
-        if token[0] in (TokensEnum.TOKEN_PLUS, TokensEnum.TOKEN_MINUS):
-            factor = self.factor(index+1, tokens)
-            return UnaryOperationNode(token, factor)
-
         # Check for INT or FLOAT
-        elif token[0] in (TokensEnum.TOKEN_INT, TokensEnum.TOKEN_FLOAT):
-            index += 1
-            return NumberNode(token)
+        if token[0] in (TokensEnum.TOKEN_INT, TokensEnum.TOKEN_FLOAT):
+            return NumberNode(token), index
 
         # Check for (
         elif token[0] == TokensEnum.TOKEN_LPAREN:
-            #print("index bij expr",index)
-            expr = self.expression(index+1, tokens)
-            return expr
+            expr, new_index = self.expression(index+1, tokens)
+            # Check for )
+            if tokens[new_index][0] == TokensEnum.TOKEN_RPAREN:
+                return expr, new_index
+            else:
+                raise Exception("No ) found", tokens[new_index], new_index)
 
-    def term(self, index: int, tokens: list):
+    def term(self, index: int, tokens: list) -> Tuple[Node,int]:
+        # A term is a mutiply or devision node
+
         # Left side of operation
-        left = self.factor(index, tokens)
-        return self.binary_operation(self.factor, (TokensEnum.TOKEN_MULTIPLY, TokensEnum.TOKEN_DIVIDE), left, index+1, tokens)
+        left, new_index = self.factor(index, tokens)
+        return self.binary_operation(self.factor, (TokensEnum.TOKEN_MULTIPLY, TokensEnum.TOKEN_DIVIDE), left, new_index+1, tokens)
 
-    def expression(self, index: int, tokens: list):
-        left= self.term(index, tokens)
-        return self.binary_operation(self.term, (TokensEnum.TOKEN_PLUS, TokensEnum.TOKEN_MINUS), left, index+1, tokens)
+    def expression(self, index: int, tokens: list) -> Tuple[Node,int]:
+        # An expression is a plus or minus node
+        left, void_index = self.term(index, tokens)
+        return self.binary_operation(self.term, (TokensEnum.TOKEN_PLUS, TokensEnum.TOKEN_MINUS), left, void_index, tokens)
 
     #Using func as decorator
-    def binary_operation(self, func, operations: tuple, left: NumberNode, index: int, tokens: list):
-        #print("Token index: ",tokens[index])
+    def binary_operation(self, func, operations: tuple, left: Union[NumberNode, BinaryOperationNode], index: int, tokens: list) -> Tuple[Node,int]:
         if tokens[index][0] not in operations:
-            #print("left:",left," Index: ", index)
-            return left
+            return left, index
         else:
             operation_token = tokens[index]
-            right = func(index+1, tokens)
+            right, void_index = func(index+1, tokens)
             left = BinaryOperationNode(left, operation_token, right)
             return self.binary_operation(func, operations, left, index+2, tokens)
