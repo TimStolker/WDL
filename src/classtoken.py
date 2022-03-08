1ea6bd67845c9bba719194fbc6b1ce3eeb6818b4
TimStolker <60575792+TimStolker@users.noreply.github.com>
2022-03-08 11:50:03 +0100

    Update classtoken.py

--- src/classtoken.py
+++ src/classtoken.py
@@ -1,32 +1,34 @@
+from enum import Enum
+from typing import Type
 #########################################
 # TOKENS
 #########################################
 
-TOKEN_INT = 'NUMMER'
-TOKEN_FLOAT = 'SCHWEBER'
-TOKEN_CHAR = 'CHARAKTER'
-TOKEN_PLUS = 'PLUS'
-TOKEN_MINUS = 'MINUS'
-TOKEN_MULTIPLY = 'MULTIPLIZIEREN'
-TOKEN_DIVIDE = 'TEILEN'
-TOKEN_LPAREN = 'LKLAMMER'
-TOKEN_RPAREN = 'RKLAMMER'
-TOKEN_EQUAL = 'GLEICH'
-TOKEN_LBRACKET = 'ECKIGELKLAMMER'
-TOKEN_RBRACKET = 'ECKIGERKLAMMER'
-TOKEN_GREATER = 'GRÖSSER'
-TOKEN_LESSER = 'WENIGER'
-TOKEN_COMMA = 'KOMMA'
-TOKEN_ERROR = 'ERROR'
-TOKEN_EOF = 'ENDE DER DATEI'
+
+class TokensEnum(Enum):
+    TOKEN_INT = 'NUMMER'
+    TOKEN_FLOAT = 'SCHWEBER'
+    TOKEN_CHAR = 'CHARAKTER'
+    TOKEN_PLUS = 'PLUS'
+    TOKEN_MINUS = 'MINUS'
+    TOKEN_MULTIPLY = 'MULTIPLIZIEREN'
+    TOKEN_DIVIDE = 'TEILEN'
+    TOKEN_LPAREN = 'LKLAMMER'
+    TOKEN_RPAREN = 'RKLAMMER'
+    TOKEN_EQUAL = 'GLEICH'
+    TOKEN_GREATER = 'GRÖSSER'
+    TOKEN_LESSER = 'WENIGER'
+    TOKEN_ERROR = 'ERROR'
+    TOKEN_EOF = 'ENDE DER DATEI'
 
 
 class Token:
-    def __init__(self, type_: str, value: str) -> None:
+    def __init__(self, type_: TokensEnum, value: str) -> None:
         self.type = type_
         self.value = value
 
-    def __repr__(self):
-        if self.value:
-            return f'{self.type}:{self.value}'
-        return f'{self.type}'
+    def __str__(self) -> str:
+        return f'({self.type}, {self.value})'
+
+    def __repr__(self) -> str:
+        return self.__str__()
