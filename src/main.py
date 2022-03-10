45cf321bd4114295863cc27ec1aaea3fabe0e8d3
TimStolker <60575792+TimStolker@users.noreply.github.com>
2022-03-10 21:21:07 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 import classlexer
-from classparser import Parser
+from classparser import *
 
 filename = "codeInput.txt"
 f = open(filename, "r")
 text = f.read()
 
-commandline = True
+commandline = False
 
 if(commandline):
     while True:
         text = input('Input>> ')
         print(classlexer.run('commandline', text))
 else:
-    print(classlexer.run(filename, text))
+    print("output: ",classlexer.run(filename, text))
