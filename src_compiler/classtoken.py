ce3dd3a8c1df89032cfb7123c2cf65bf71f6a94a
TimStolker <60575792+TimStolker@users.noreply.github.com>
2022-03-25 15:06:06 +0100

    Create classtoken.py

---
+++ src_compiler/classtoken.py
@@ -0,0 +1,62 @@
+from enum import Enum
+#########################################
+# TOKENS
+#########################################
+
+
+class TokensEnum(Enum):
+    # Special tokens
+    TOKEN_INT           = 'NUMMER'
+    TOKEN_FLOAT         = 'SCHWEBER'
+    TOKEN_PLUS          = '+'
+    TOKEN_MINUS         = '-'
+    TOKEN_MULTIPLY      = '*'
+    TOKEN_DIVIDE        = '/'
+    TOKEN_LPAREN        = '('
+    TOKEN_RPAREN        = ')'
+    TOKEN_EQUAL         = '='
+    TOKEN_BEGIN_FUNCTION= '{'
+    TOKEN_END_FUNCTION  = '}'
+    TOKEN_COMMA         = ','
+
+    TOKEN_ERROR         = 'ERROR'
+    TOKEN_EOF           = 'EOF'
+    TOKEN_NAME = 'NAME'
+
+    # Conditional tokens
+    TOKEN_GREATER       = '>'
+    TOKEN_LESSER        = '<'
+    TOKEN_GREATER_EQUAL = '>='
+    TOKEN_LESSER_EQUAL  = '<='
+    TOKEN_DOUBLE_EQUAL  = '=='
+
+    # Reserved words
+    IF = 'ALS'
+    ELSE_IF = 'ANDALS'
+    ELSE = 'ANDERS'
+    THEN = 'DANN'
+    WHILE = 'WAHREND'
+    LOOP = 'WIEDERHOLEN'
+    FUNCTION = 'FUNKTION'
+    VAR = 'VAR'
+    ENDE = 'ENDE'
+    RETURN = 'RETURN'
+
+    # End words
+    SLA = 'SLA'
+    NELOHREDEIW = 'NELOHREDEIW'
+
+
+class Token:
+    # __init__ :: None
+    def __init__(self, token_type: TokensEnum, value: str) -> None:
+        self.type = token_type
+        self.value = value
+
+    # __str__ :: str
+    def __str__(self) -> str:
+        return f'({self.type}, {self.value})'
+
+    # __repr__ :: str
+    def __repr__(self) -> str:
+        return self.__str__()
